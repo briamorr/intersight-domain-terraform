96243516cc4df79f2a4294d9ed1f9ca87d9a407b
briamorr <78086763+briamorr@users.noreply.github.com>
2026-03-20 14:24:31 -0400

    Update policy_server_firmware.tf

--- policy_server_firmware.tf
+++ policy_server_firmware.tf
@@ -23,34 +23,3 @@ resource "intersight_firmware_policy" "August2023" {
     bundle_version = "4.2(3e)"
   }
 }
-
-# This data resource and firmware policy dynamically creates a set of firmware policies
-# for each server family with the version set to the recommended version
-data "intersight_firmware_distributable" "recommended" {
-  recommended_build = "Y"
-  import_state      = "Imported"
-  image_type        = "Intersight Managed Mode Server"
-}
-
-resource "intersight_firmware_policy" "recommended" {
-  for_each = { for i in data.intersight_firmware_distributable.recommended.results : "${i.moid}" => i }
-  name     = each.value.mdfid
-  dynamic "tags" {
-    for_each = local.tags
-    content {
-      key   = tags.key
-      value = tags.value
-    }
-  }
-  organization {
-    moid = local.organization
-  }
-
-  target_platform = "FIAttached"
-
-  model_bundle_combo {
-    model_family   = trimsuffix(trimsuffix(trimsuffix(each.value.supported_models[0], "X"), "N"), "S")
-    bundle_version = each.value.nr_version
-  }
-
-}
